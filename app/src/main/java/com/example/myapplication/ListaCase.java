b8013c81fa4797fe26e68dedbf212c9f4c8b8ebb
CampanioloNF <s226878@studenti.polito.it>
2021-05-14 17:08:24 +0200

    Aggiornamento

--- app/src/main/java/com/example/myapplication/ListaCase.java
+++ app/src/main/java/com/example/myapplication/ListaCase.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//ciao
 public class ListaCase extends AppCompatActivity {
 
     @Override
