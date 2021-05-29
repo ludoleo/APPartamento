d2c3119ac08d286e54b7d1e0f7463e68dce4a8c3
GioeleBarella <barellagioele@gmail.com>
2021-05-29 16:46:30 +0200

    Modifica layout recensioni con Rating Bar e Codice per immagine Studente

--- app/src/main/java/com/example/myapplication/profilo/ProfiloStudente.java
+++ app/src/main/java/com/example/myapplication/profilo/ProfiloStudente.java
@@ -1,22 +1,46 @@
 package com.example.myapplication.profilo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.home.Home;
 import com.example.myapplication.recensione.RecensioniStudentInterne;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class ProfiloStudente extends AppCompatActivity {
 
     Button recensioni;
     Button modifica;
-    ImageButton imagebutton2;
+    Button changeprofilo;
+    ImageView immaginestudente;
+
+
+    FirebaseStorage storage;
+     StorageReference storageReference;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +64,73 @@ public class ProfiloStudente extends AppCompatActivity {
                 startActivity(a);
             }
         });
+
+        changeprofilo = findViewById(R.id.changeimage);
+
+        // nel file c'è salvato Firbase user --> in teoria dovrebbe essere il nostro studenti
+
+        //StorageReference profileref = storageReference.child("user/"+f.getCurrentuser().getUid()+"profile.jpg"); //--> stesso percorso da copiare in uploadFirebase al posto di image
+        //profileref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+          //  @Override
+            //public void onSuccess(Uri uri) {
+            // Picasso.get().load(uri).into(immaginestudente);
+
+
+            //}
+       // });
+
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+
+        immaginestudente = findViewById(R.id.immaginestudente);
+        changeprofilo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                 Intent opengalleryintent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                 startActivityForResult(opengalleryintent,1000);
+          
+            }
+
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode== 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                immaginestudente.setImageURI(imageUri);
+                uploadimagetoFirebase(imageUri);
+            }
+        }
+    }
+
+    private void uploadimagetoFirebase(Uri imageUri) {
+            // upload image to Firebase storage
+      StorageReference fileRef = storageReference.child("profile.jpg");
+      fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+          @Override
+          public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+            Toast.makeText(ProfiloStudente.this,"Image Uploaded",Toast.LENGTH_SHORT).show();
+             // fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() { DEVO MODIFICARE buil.gradle Module agg Picasso con --> implementation 'com.squareup.picasso:picasso:2.71828'
+               //   @Override
+                 // public void onSuccess(Uri uri) {
+                   //   Picasso.get.load(uri).into(immaginestudente)
+                 // }
+             // });
+          }
+      }).addOnFailureListener(new OnFailureListener() {
+          @Override
+          public void onFailure(@NonNull Exception e) {
+              Toast.makeText(ProfiloStudente.this,"Failed",Toast.LENGTH_SHORT).show();
+
+          }
+      });
+
     }
 
     public void goHome(View view) {
@@ -48,3 +139,4 @@ public class ProfiloStudente extends AppCompatActivity {
             startActivity(intent);
     }
 }
+
