22414501308339c76392de320c0b55bfd9d9cca7
CampanioloNF <s226878@studenti.polito.it>
2021-07-12 09:50:38 +0200

    Ordinamento funziona

--- app/src/main/java/com/example/myapplication/classi/AnnuncioPrezzoComparator.java
+++ app/src/main/java/com/example/myapplication/classi/AnnuncioPrezzoComparator.java
@@ -6,9 +6,9 @@ public class AnnuncioPrezzoComparator implements Comparator<Annuncio> {
     @Override
     public int compare(Annuncio a1, Annuncio a2) {
         if(a1.getPrezzoMensile()>a2.getPrezzoMensile())
-            return -1;
-        else if(a1.getPrezzoMensile()<a2.getPrezzoMensile())
             return 1;
+        else if(a1.getPrezzoMensile()<a2.getPrezzoMensile())
+            return -1;
 
         return 0;
     }
