3457ab93feeceae267321df0e22c7fc67db69e4e
CampanioloNF <s226878@studenti.polito.it>
2021-07-16 14:32:41 +0200

    Ultimo

--- app/src/main/java/com/example/myapplication/profilo/ListaRecensioniPossibili.java
+++ app/src/main/java/com/example/myapplication/profilo/ListaRecensioniPossibili.java
@@ -1,14 +1,20 @@
 
 package com.example.myapplication.profilo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -36,6 +42,25 @@ public class ListaRecensioniPossibili extends AppCompatActivity {
         lv_recensioni_possibili_studente = (ListView) findViewById(R.id.lv_recensioni_possibili_studente);
 
         //CONTROLLO SE L'UTENTE E' UN PROPRIETARIO O UNO STUDENTE
+        myRef.child("Utenti")
+                .child("Studenti")
+                .child(user.getUid())
+                .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                        //SONO UN PROPRIETARIO E POSSO RECENSIRE SOLO STUDENTI
+                    if (task.getResult().getValue() == null) {
+
+                        //SONO UNO STUDENTE E POSSO RECENSIRE UN PROPRIETARIO, UNA CASA O UNO STUDENTE
+                    }else{
+
+                    }
+                }
+            }
+        });
 
 
     }
